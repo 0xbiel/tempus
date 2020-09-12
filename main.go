46f51b2372d91aaed89b3a4ab4edc65438f855e0
Biel <bielp07@gmail.com>
2020-09-12 09:02:02 -0300

    add logo

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	//	"flag"
+	"fmt"
+)
+
+func logo() {
+	fmt.Println(" ,                  ")
+	fmt.Println("-+- _ ._ _ ._ . . __")
+	fmt.Println(" | (/,[ | )[_)(_|_) ")
+	fmt.Println("           |        ")
+
+	fmt.Println("		~0xbiel")
+}
 
 func main() {
-	fmt.Println("tempus")
+	logo()
 }
