6f16be12ca3772a01ce9a06b3eb0784664d1e9ba
Biel <bielp07@gmail.com>
2020-09-20 12:53:45 -0300

    replace elif and shit for switch.

--- main.go
+++ main.go
@@ -6,39 +6,53 @@ import (
 )
 
 func logo() {
+  logo := `
+ ,                  
+-+- _ ._ _ ._ . . __
+ | (/,[ | )[_)(_|_) 
+           |        
+		~0xbiel
+  `
 
-	fmt.Println(" ,                  ")
-	fmt.Println("-+- _ ._ _ ._ . . __")
-	fmt.Println(" | (/,[ | )[_)(_|_) ")
-	fmt.Println("           |        ")
-
-	fmt.Println("		~0xbiel\n")
+	fmt.Println(logo)
 }
 
 func help() {
-
   logo()
-  
   fmt.Println("usage: tempus [args]\n\n")
-  fmt.Println("[-a] [add] - add new entry.\n")
-  fmt.Println("[-i] [init] - init time.\n")
-  fmt.Println("[-s] [stop] - stop time.\n")
-  fmt.Println("[-r] [remove] - remove entry.\n")
-  fmt.Println("[-l] [list] - list entries.\n")
-  fmt.Println("[-e] [export] - export activity.\n")
-  fmt.Println("[-h] [help] - show help.\n")
+  fmt.Println("[-a] - add new entry.\n")
+  fmt.Println("[-i] - init time.\n")
+  fmt.Println("[-s] - stop time.\n")
+  fmt.Println("[-r] - remove entry.\n")
+  fmt.Println("[-l] - list entries.\n")
+  fmt.Println("[-e] - export activity.\n")
+  fmt.Println("[-h] - show help.\n")
 }
 
 func main() {
-
   if (len(os.Args) < 2) {
 
-    help()
+    help();
 
-  } else if (os.Args[1] == "-a" || os.Args[1] == "add") {
+  } else {
 
-      fmt.Println("add\n");
-
-  }
+      switch os.Args[1] {
+        case "-a": 
+          fmt.Println("add")
+        case "-i":
+          fmt.Println("init")
+        case "-s":
+          fmt.Println("stop")
+        case "-r":
+          fmt.Println("remove")
+        case "-l":
+          fmt.Println("list")
+        case "-e":
+          fmt.Println("export")
+        case "-h":
+          help()
+        default:
+          help()
+      }
+    }
 }
-
